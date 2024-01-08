bc1b860f09e2348e5a1d3858954cdbf5f0247a15
Filip Cederquist <cederquist94@hotmail.com>
2024-01-08 20:33:28 +0100

    Fix bug with images not being saved

--- camera.py
+++ camera.py
@@ -7,22 +7,22 @@ class Camera:
     def __init__(self):
         self.cap = cv2.VideoCapture(0)
         self.last_frame = None
-        self.last_face_coords = None
+        self.last_faces = None
 
     def get_frame(self):
         ret, frame = self.cap.read()
         if not ret:
             return None
 
-        processed_frame, face_coords = detect_faces(frame)
+        processed_frame, faces = detect_faces(frame)
         self.last_frame = processed_frame
-        self.last_face_coords = face_coords
+        self.last_faces = faces
 
         _, jpeg = cv2.imencode(".jpg", processed_frame)
         return jpeg.tobytes()
 
     def get_last_frame_and_face_coords(self):
-        return self.last_frame, self.last_face_coords
+        return self.last_frame, self.last_faces
 
     def __del__(self):
         self.cap.release()
--- main.py
+++ main.py
@@ -12,14 +12,11 @@ import os
 
 app = FastAPI()
 camera = Camera()
-save_dir = "data/1"
+save_dir = 'data/2'
 os.makedirs(save_dir, exist_ok=True)
 
-import torch
 
 
-def load_embeddings_from_file(file_path):
-    return torch.load(file_path).numpy()
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -30,6 +27,7 @@ def read_root():
     return HTMLResponse(content=html_content)
 
 
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
@@ -37,23 +35,19 @@ async def websocket_endpoint(websocket: WebSocket):
         frame = camera.get_frame()
         if frame is not None:
             # Convert frame to base64 for web compatibility
-            encoded_frame = base64.b64encode(frame).decode("utf-8")
+            encoded_frame = base64.b64encode(frame).decode('utf-8')
             await websocket.send_text(encoded_frame)
-
 
 @app.post("/save_image")
 def save_image():
-    frame, face_coords = camera.get_last_frame_and_face_coords()
-    if frame is None or face_coords is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="No face detected"
-        )
-
-    for i, (x0, y0, x1, y1) in enumerate(face_coords):
-        cropped_face = frame[int(y0) : int(y1), int(x0) : int(x1)]
-        filename = (
-            f"data/1/face_{i}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.jpg"
-        )
+    frame, faces = camera.get_last_frame_and_face_coords()
+    if frame is None or faces is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No face detected")
+    for i, face in enumerate(faces):
+        face_coords, label = face
+        (x0, y0, x1, y1) = face_coords
+        cropped_face = frame[int(y0):int(y1), int(x0):int(x1)]
+        filename = f"data/2/face_{i}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.jpg"
         cv2.imwrite(filename, cropped_face)
 
     return {"message": f"{len(face_coords)} face(s) saved"}
@@ -61,3 +55,5 @@ def save_image():
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
